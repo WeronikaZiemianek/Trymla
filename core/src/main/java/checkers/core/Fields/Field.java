e11710e7775de90f9d870b0389e93e9c25207137
kasiaMarek <kasia@marek.net>
2017-12-03 22:46:08 +0100

    docs update

--- core/src/main/java/checkers/core/Fields/Field.java
+++ core/src/main/java/checkers/core/Fields/Field.java
@@ -4,24 +4,6 @@ import checkers.core.Checker;
 
 public class Field {
     private Checker occupiedBy;
-
-    Field() {
-        init();
-    }
-
-    private void init() {
-        occupiedBy = Checker.EMPTY;
-    }
-
-    Checker getOccupiedBy() {
-        return occupiedBy;
-    }
-
-    void setOccupiedBy(Checker occupiedBy) {
-        this.occupiedBy = occupiedBy;
-    }
-
-    Checker getTypeOfField() {
-        return Checker.EMPTY;
-    }
+    private Checker type;
+    
 }
