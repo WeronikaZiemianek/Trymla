ffe1735830d111654b94a9f90794d9bd94bf0084
Wera <woziemniak@gmail.com>
2017-12-21 00:30:13 +0100

    Add DefaultGamesManager body

--- server/src/main/java/checkers/server/game/DefaultGamesManager.java
+++ server/src/main/java/checkers/server/game/DefaultGamesManager.java
@@ -2,26 +2,35 @@ package checkers.server.game;
 
 import checkers.core.PlayerFactory;
 import checkers.core.RemotePlayer;
+import checkers.core.boards.BoardFactory;
+import checkers.core.boards.RegularBoardFactory;
 import checkers.server.player.DefaultPlayerFactory;
 import checkers.server.Connection;
 
 import java.rmi.RemoteException;
 
 public class DefaultGamesManager implements GamesManager {
-    private PlayerFactory factory;
+    private PlayerFactory playerFactory;
     private Connection connection;
+    private BoardFactory boardFactory;
+    private Game game;
 
-    public DefaultGamesManager(){
+    public DefaultGamesManager() throws RemoteException {
         try {
-            factory = new DefaultPlayerFactory(this);
+            playerFactory = new DefaultPlayerFactory(this);
         } catch(RemoteException e) {
             e.printStackTrace();
         }
-        connection = new Connection(factory);
+        connection = new Connection(playerFactory);
+
+        // To zamknąć i obsługiwac w UI
+        boardFactory = new RegularBoardFactory();
+        game = new RegularGame(boardFactory.createNewBoard(6));
     }
 
     @Override
     public void addPlayer(RemotePlayer player) {
+
         connection.putPlayer(player);
     }
 
