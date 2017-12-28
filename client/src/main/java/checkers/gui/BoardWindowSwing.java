9f550767a41c4c6047f4f0420eea8b5690c3d3ee
Wera <woziemniak@gmail.com>
2017-12-28 12:07:25 +0100

    Board Gui

--- client/src/main/java/checkers/gui/BoardWindowSwing.java
+++ client/src/main/java/checkers/gui/BoardWindowSwing.java
@@ -1,90 +1,80 @@
-package checkers.client.gui;
+package checkers.gui;
 
 import checkers.core.Checker;
 import checkers.core.Coordinates;
 import checkers.core.boards.Board;
-import checkers.core.boards.RegularBoard;
-import checkers.core.boards.WrongMoveException;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class BoardWindowSwing extends JFrame{
-Board board;
-    public BoardWindowSwing() {
+public class BoardWindowSwing extends JFrame {
+    Board board;
+    Color red = Color.RED;
+    Color green = Color.GREEN;
+    Color blue = Color.BLUE;
+    Color black = Color.BLACK;
+    Color yellow = Color.YELLOW;
+    Color white = Color.WHITE;
+    Color orange = Color.ORANGE;
 
-
-        // COLORS
-        Color red = Color.RED;
-        Color green = Color.GREEN;
-        Color blue = Color.BLUE;
-        Color black = Color.BLACK;
-        Color yellow = Color.YELLOW;
-        Color white = Color.WHITE;
-        Color orange = Color.ORANGE;
+    public BoardWindowSwing(int rows, int cols) {
 
         this.setTitle("MyWarcab");
         this.setLayout(new GridLayout(17, 25));
         this.setSize(650, 650);
         this.setVisible(true);
 
-        JButton button = null;
-
-        for(int i = 0; i < 25; i++){
+        Container pane = getContentPane();
+        pane.setLayout(new GridLayout(rows, cols));
+        for (int i = 0; i < 25; i++) {
             for (int j = 0; j < 17; j++) {
-                if (board.getFieldOccupiedBy(new Coordinates(i, j)) == Checker.EMPTY) {
-                    button = new JButton();
-                    button.setBackground(orange);
-                    add(button);
-                }
-                else{
+                {
+                    JButton button = new JButton(Integer.toString(i + 1 + j));
 
-                        button = new JButton();
-                        switch (board.getFieldType(new Coordinates(i, j))) [
-                        case Color.WHITE
-                        {
-                            button.setBackground(white);
-                            break;
-                        }
-                        case Color.RED
-                        {
-                            button.setBackground(red);
-                            break;
-                        }
-                        case Color.YELLOW
-                        {
-                            button.setBackground(yellow);
-                            break;
-                        }
-                        case Color.BLUE
-                        {
-                            button.setBackground(blue);
-                            break;
-                        }
-                        case Color.BLACK
-                        {
-                            button.setBackground(black);
-                            break;
-                        }
-                        case Color.GREEN
-                        {
-                            button.setBackground(green);
-                            break;
-                        }
+                    if (board.getFieldOccupiedBy(new Coordinates(i, j)) == Checker.EMPTY) {
+                        button.setBackground(orange);
+                    } else {
 
-                        default {}
-
-
+                        switch (board.getFieldType(new Coordinates(i, j))) {
+                            case WHITE:{
+                                button.setBackground(white);
+                                break;
+                            }
+                            case RED: {
+                                button.setBackground(red);
+                                break;
+                            }
+                            case YELLOW: {
+                                button.setBackground(yellow);
+                                break;
+                            }
+                            case BLUE: {
+                                button.setBackground(blue);
+                                break;
+                            }
+                            case BLACK: {
+                                button.setBackground(black);
+                                break;
+                            }
+                            case GREEN: {
+                                button.setBackground(green);
+                                break;
+                            }
+                        }
                     }
-                    ]
+                    pane.add(button);
+                }
             }
         }
-
-
-    public static void main(String[] args) {
-
-        String title = "My Chess Board";
-
-        ChessBoard chessBoard = new ChessBoard(title); // Creating the instance of chess board
     }
+
+    public static void main() {
+        int rows = 25;
+        int cols = 17;
+        BoardWindowSwing gt = new BoardWindowSwing(rows, cols);
+        gt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gt.pack();
+        gt.setVisible(true);
+    }
+
 }
